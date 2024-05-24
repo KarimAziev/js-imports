2f515023c44a2b5d76f0ca97da987caa11a3ba8e
KarimAziev <karim.aziiev@gmail.com>
2024-05-24 17:46:12 +0300

    build: bump minimum Emacs version to 27.1
    Updated the `Package-Requires` field in `js-imports.el` to require
    Emacs version 27.1 instead of 26.1.

--- js-imports.el
+++ js-imports.el
@@ -6,7 +6,7 @@
 ;; URL: https://github.com/KarimAziev/js-imports
 ;; Keywords: languages
 ;; Version: 1.2.0
-;; Package-Requires: ((emacs "26.1"))
+;; Package-Requires: ((emacs "27.1"))
 ;; SPDX-License-Identifier: GPL-3.0-or-later
 
 ;; This file is NOT part of GNU Emacs.
